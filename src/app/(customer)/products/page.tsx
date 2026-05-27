8f5118c3c39bcb73950d02a1ae145172a1e87d01
afridho-ikhsan <afridhoikhsan@gmail.com>
2026-05-27 21:43:42 +0700

    STYLE(CATALOG PAGE): Banner adjustment

--- src/app/(customer)/products/page.tsx
+++ src/app/(customer)/products/page.tsx
@@ -17,8 +17,8 @@ async function ListPage(props: {
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative py-3">
       <CatalogOfflineBar />
-      <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64 mb-12">
-        <div className="w-2/3 flex flex-col items-center justify-center gap-8">
+      <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64 mb-12 relative">
+        {/* <div className="w-2/3 flex flex-col items-center justify-center gap-8 relative z-10">
           <h1 className="text-3xl text-center font-semibold leading-[48px] text-gray-700">
             Dapatkan produk menarik dengan harga yang lebih terjangkau
           </h1>
@@ -28,10 +28,10 @@ async function ListPage(props: {
           >
             Cek Sekarang
           </Link>
-        </div>
-        <div className="relative w-1/3">
-          <Image src="/woman.png" alt="" fill className="object-contain" />
-        </div>
+        </div> */}
+        <Link href="#daftar-produk" className="absolute inset-0 w-full cursor-pointer">
+          <Image src="/catalog-banner.png" alt="" fill className="object-contain" />
+        </Link>
       </div>
 
       <Filter />
